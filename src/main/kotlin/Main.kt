2f53d5abffbfeeb9fb67d777c355482cc2175055
Nicolas <nicolas@wagener.family>
2024-10-03 13:56:16 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+package org.example
+
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.TextColor
+import com.googlecode.lanterna.gui2.*
+import com.googlecode.lanterna.screen.Screen
+import com.googlecode.lanterna.screen.TerminalScreen
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    // Setup terminal and screen layers
+    val terminal = DefaultTerminalFactory().createTerminal()
+    val screen: Screen = TerminalScreen(terminal)
+    screen.startScreen()
+
+    // Create panel to hold components
+    val panel = Panel()
+    panel.setLayoutManager(GridLayout(2))
+
+    panel.addComponent(Label("Forename"))
+    panel.addComponent(TextBox())
+
+    panel.addComponent(Label("Surname"))
+    panel.addComponent(TextBox())
+
+    panel.addComponent(EmptySpace(TerminalSize(0, 0))) // Empty space underneath labels
+    panel.addComponent(Button("Submit"))
+
+    // Create window to hold the panel
+    val window = BasicWindow()
+    window.component = panel
+
+    // Create gui and start gui
+    val gui = MultiWindowTextGUI(screen, DefaultWindowManager(), EmptySpace(TextColor.ANSI.BLUE))
+    gui.addWindowAndWait(window)
+}
